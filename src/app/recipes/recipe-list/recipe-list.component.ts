c83288c7bc4e55446e3c766f66fca199ca83dcd4
vcc1432 <valerie.gersen@gmail.com>
2018-12-02 22:15:02 +0100

    Working recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://c1.staticflickr.com/3/2356/5778822316_9360c4ebfd_b.jpg'),
+    new Recipe('A Second Test Recipe', 'Another description', 'https://c1.staticflickr.com/5/4253/34578618823_b9de950c64_b.jpg')
+  ]
 
   constructor() { }
 
