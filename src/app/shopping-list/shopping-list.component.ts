dfe8c37081d443c4b14c6073e428b687dce1337f
vcc1432 <valerie.gersen@gmail.com>
2019-06-10 22:31:21 +0200

    Emitters to observables

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,25 +1,31 @@
 import { ShoppingListService } from './shopping-list.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { Subscription } from '../../../node_modules/rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[] = [];
+  private subscriptions = new Subscription();
 
   constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit() {
     this.ingredients = this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientsChanged
+    this.subscriptions.add(this.shoppingListService.ingredientsChanged
       .subscribe(
         ((ingredients: Ingredient[]) => {
           this.ingredients = ingredients;
         } )
-      );
+      )
+    );
+  }
 
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
   }
 }
